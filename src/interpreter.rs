51d7d7f08f1eca1706213c550a436596aa8ea13d
e4h2n <eshanbajwa@gmail.com>
2026-09-05 19:01:29 -0700

    lets and lambdas but envs are kinda fucked

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,12 +1,25 @@
 use crate::data::*;
 use crate::parser::*;
+
 use std::collections;
 use std::fmt;
+use std::ops::BitOr;
 use std::rc;
 
 #[derive(Clone)]
 pub struct Environment {
     bindings: collections::HashMap<String, Bindee>,
+}
+impl BitOr for Environment {
+    type Output = Self;
+    fn bitor(self, rhs: Self) -> Self::Output {
+        let mut new_env = Environment {
+            bindings: collections::HashMap::new(),
+        };
+        new_env.bindings.extend(self.bindings);
+        new_env.bindings.extend(rhs.bindings);
+        new_env
+    }
 }
 
 pub type Closure = dyn Fn(AstNode, Environment) -> Result<Bindee, InterpreterError>;
@@ -30,23 +43,29 @@ impl fmt::Debug for Bindee {
 }
 
 #[derive(Debug)]
-pub struct InterpreterError;
+pub struct InterpreterError(String);
 
 impl AstNode {
     pub fn eval(&self, environment: &Environment) -> Result<Bindee, InterpreterError> {
+        println!("{:?}", environment.bindings.keys());
         return match self {
             AstNode::Nil => Ok(Bindee::Nil),
             AstNode::Primitive(primitive) => Ok(Bindee::Value(*primitive)),
             AstNode::Identifier(identifier) => environment
                 .bindings
                 .get(identifier)
-                .ok_or(InterpreterError)
+                .ok_or(InterpreterError(format!(
+                    "No binding found for identifier {}",
+                    identifier
+                )))
                 .cloned(),
             AstNode::Pair(car, cdr) => {
-                if let Ok(Bindee::Closure(closure)) = car.eval(environment) {
+                if let Bindee::Closure(closure) = car.eval(environment)? {
                     closure(*cdr.clone(), environment.clone())
                 } else {
-                    Err(InterpreterError)
+                    Err(InterpreterError(
+                        "Tried to evaluate pair but car was not a closure!".to_string(),
+                    ))
                 }
             }
         };
@@ -58,35 +77,68 @@ pub fn starting_env() -> Environment {
     let mut bindings = collections::HashMap::new();
     bindings.insert(
         "let".to_string(),
-        Bindee::Closure(rc::Rc::new(|id_val: AstNode, env: Environment| {
-            let AstNode::Pair(id, val) = id_val else {
-                return Err(InterpreterError);
+        Bindee::Closure(rc::Rc::new(|id: AstNode, env: Environment| {
+            let AstNode::Identifier(id) = id else {
+                return Err(InterpreterError(
+                    "Tried to bind non-identifier!".to_string(),
+                ));
             };
-            let AstNode::Identifier(id) = *id else {
-                return Err(InterpreterError);
-            };
-            let val = val.eval(&env)?;
             Ok(Bindee::Closure(rc::Rc::new(
-                move |exec: AstNode, env: Environment| {
-                    let mut new_env = env.clone();
-                    new_env.bindings.insert(id.clone(), val.clone());
-                    return exec.eval(&new_env);
+                move |value: AstNode, _: Environment| {
+                    let id = id.clone();
+                    let val = value.eval(&env)?;
+                    Ok(Bindee::Closure(rc::Rc::new(
+                        move |todo: AstNode, env: Environment| {
+                            let mut new_env = env.clone();
+                            new_env.bindings.insert(id.clone(), val.clone());
+                            todo.eval(&new_env)
+                        },
+                    )))
                 },
             )))
         })),
     );
 
     bindings.insert(
+        "lambda".to_string(),
+        Bindee::Closure(rc::Rc::new(|id: AstNode, env: Environment| {
+            let AstNode::Identifier(id) = id else {
+                return Err(InterpreterError(
+                    "Tried to write non-identifier function argument!".to_string(),
+                ));
+            };
+            Ok(Bindee::Closure(rc::Rc::new(
+                move |todo: AstNode, _: Environment| {
+                    let id = id.clone();
+                    let env = env.clone();
+                    Ok(Bindee::Closure(rc::Rc::new(
+                        move |value: AstNode, _: Environment| {
+                            let mut new_env = env.clone();
+                            new_env.bindings.insert(id.clone(), value.eval(&env)?);
+                            todo.eval(&new_env)
+                        },
+                    )))
+                },
+            )))
+        })),
+    );
+
+    bindings.insert(
+        // ((+ a) b)
         "+".to_string(),
         Bindee::Closure(rc::Rc::new(|x: AstNode, env: Environment| {
             match x.eval(&env)? {
                 Bindee::Value(Value::Number(x)) => Ok(Bindee::Closure(rc::Rc::new(
                     move |y: AstNode, env: Environment| match y.eval(&env)? {
                         Bindee::Value(Value::Number(y)) => Ok(Bindee::Value(Value::Number(x + y))),
-                        _ => Err(InterpreterError),
+                        _ => Err(InterpreterError(
+                            "Second argument of '+' was non-numeric!".to_string(),
+                        )),
                     },
                 ))),
-                _ => Err(InterpreterError),
+                _ => Err(InterpreterError(
+                    "First argument of '+' was non-numeric!".to_string(),
+                )),
             }
         })),
     );
